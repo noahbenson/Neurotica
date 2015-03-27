ebbea51fae823d8b6c0604fb93b2addc3efd89ee
Noah C. Benson <nben@nyu.edu>
2015-03-27 12:31:16 -0400

    NifTI importing now supports NifTI-1 and NifTI-2 formats and correctly reads in Extension data. So far, CifTI extension data is parsed as XML (but not interpreted) and other data is read as binary unsigned bytes.

--- src/Neurotica/NifTI.m
+++ src/Neurotica/NifTI.m
@@ -158,6 +158,9 @@ NifTIColorTranslate[type_String, data_List] := If[
   With[
     {bits = ToExpression @ StringTake[type, {4, -1}]},
     IntegerDigits[BitAnd[data, 2^bits - 1], 256]]];
+NifTIColorTranslate[type_Integer, data_List] := NifTIColorTranslate[
+  NifTIDatatypeTranslate[type],
+  data];
 NifTIColorUntranslate[type_String, data_List] := If[
   StringLength[type] < 3 || StringTake[type, 3] != "RGB",
   data,
@@ -166,6 +169,9 @@ NifTIColorUntranslate[type_String, data_List] := If[
     With[
       {u = If[bits == 24, {256^2, 256, 1}, {256^3, 256^2, 256, 1}]},
       Map[Dot[#,u]&, data, {-1}]]]];
+NifTIColorUntranslate[type_Integer, data_List] := NifTIColorUntranslate[
+  NifTIDatatypeTranslate[type],
+  data];
 Protect[NifTIDatatypeTranslate, NifTIDatatypeUntranslate,
         NifTIDataToBinaryType,
         NifTIColorTranslate, NifTIColorUntranslate];
@@ -233,6 +239,11 @@ NifTIUnitTranslate[unit_] := BitOr[
     "RadiansPerSecond", 48]];
 Protect[NifTIUnitTranslate, NifTIUnitUntranslate];
 
+(* NifTI Header Extension codes *******************************************************************)
+$NifTIExtensionCodeCifTI = 32;
+Protect[$NifTIExtensionCodeCifTI];
+
+
 (* ============================================================================================== *)
 (* Here we include the NifTI file spec header:
    int   sizeof_hdr;    /*!< MUST be 348           */  /* int sizeof_hdr;      */    0-3,
@@ -290,12 +301,15 @@ Protect[NifTIUnitTranslate, NifTIUnitUntranslate];
    char magic[4] ;      /*!< MUST be "ni1\0" or "n+1\0". */                                       *)
 (* ============================================================================================== *)
 
-$NIFTIHeaderSize = 348;
-$MinNIFTIOffset = 352;
-Protect[$NIFTIHeaderSize, $MinNIFTIOffset];
+$NifTI1HeaderSize = 348;
+$NifTI2HeaderSize = 540;
+$MinNifTI1Offset = 352;
+$MinNifTI2Offset = 544;
+Protect[$NifTI1HeaderSize, $NifTI2HeaderSize, $MinNifTI1Offset, $MinNifTI2Offset];
 
 (* #ImportNifTIHeader *****************************************************************************)
-ImportNifTIHeader[stream_, opts___Rule] := Check[
+
+ImportNifTI1Header[stream_, opts___Rule] := Check[
   SetStreamPosition[stream, 40];
   Block[
     {$ByteOrdering = If[0 < BinaryRead[stream, "Integer16"] < 8, 1, -1] * $ByteOrdering},
@@ -304,7 +318,7 @@ ImportNifTIHeader[stream_, opts___Rule] := Check[
       {raw = ReadBinaryStructure[
          stream,
          {"HeaderSize" -> {"Integer32", 1, Function[
-            If[#[[1]] != $NIFTIHeaderSize, 
+            If[#[[1]] != $NifTI1HeaderSize, 
               Message[ImportNifTI::badfmt, "Header does not start with 348"],
               #[[1]]]]},
           "DataType" -> {"Integer8", 10},
@@ -320,7 +334,7 @@ ImportNifTIHeader[stream_, opts___Rule] := Check[
           "BitsPerVoxel" -> "Integer16",
           "SliceStart" -> "Integer16",
           "GridSpacings" -> {"Real32", 8},
-          "VoxelOffset" -> {"Real32", 1, Min[{#[[1]], $MinNIFTIOffset}]&},
+          "VoxelOffset" -> {"Real32", 1, Max[{#[[1]], $MinNifTI1Offset}]&},
           "ScaleSlope" -> "Real32",
           "ScaleIntercept" -> "Real32",
           "SliceEnd" -> "Integer16",
@@ -340,19 +354,155 @@ ImportNifTIHeader[stream_, opts___Rule] := Check[
           "MagicTerminus" -> {"Character8", 4, Function[
             With[
               {str = BinaryStringFix[#]},
-              If[str == "nil" || str == "n+1", 
+              If[str == "ni1" || str == "n+1", 
                 str,
-                Message[ImportNifTI::badfmt, "header does not end in nil or n+1 string"]]]]}}]},
+                Message[ImportNifTI::badfmt, "header does not end in ni1 or n+1 string"]]]]}}]},
       If[raw === $Failed,
         raw,
-        "Header" -> Append[
+        "Header" -> Join[
           With[
             {dims = "Dimensions" /. raw},
             Replace[
               raw, 
               ("GridSpacings" -> gs_) :> ("GridSpacings" -> gs[[2 ;; (Length[dims] + 1)]]),
               {1}]],
-          "ByteOrdering" -> $ByteOrdering]]]],
+          {"ByteOrdering" -> $ByteOrdering,
+           "Extension" -> With[
+             {ext = NestWhileList[
+                Function @ With[
+                  {esize = BinaryRead[stream, "Integer32"],
+                   ecode = BinaryRead[stream, "Integer32"]},
+                  {BinaryReadList[stream, "UnsignedInteger8", esize - 12],
+                   BinaryReadList[stream, "Integer8", 4]}],
+                {{}, BinaryReadList[stream, "Integer8", 4]},
+                #[[2,1]] != 0 &]},
+             If[Length[ext] == 1, None, ext[[2;;All, 1]]]]}]]]],
+  $Failed];
+
+(* Here we have the NifTI-2 header:
+                         /***************************/ /**********************/ /************/
+struct nifti_2_header {  /* NIFTI-2 usage           */ /* NIFTI-1 usage      */ /*  offset  */
+                         /***************************/ /**********************/ /************/
+   int   sizeof_hdr;     /*!< MUST be 540           */ /* int sizeof_hdr; (348) */   /*   0 */
+   char  magic[8] ;      /*!< MUST be valid signature. */  /* char magic[4];    */   /*   4 */
+   int16_t datatype;     /*!< Defines data type!    */ /* short datatype;       */   /*  12 */
+   int16_t bitpix;       /*!< Number bits/voxel.    */ /* short bitpix;         */   /*  14 */
+   int64_t dim[8];       /*!< Data array dimensions.*/ /* short dim[8];         */   /*  16 */
+   double intent_p1 ;    /*!< 1st intent parameter. */ /* float intent_p1;      */   /*  80 */
+   double intent_p2 ;    /*!< 2nd intent parameter. */ /* float intent_p2;      */   /*  88 */
+   double intent_p3 ;    /*!< 3rd intent parameter. */ /* float intent_p3;      */   /*  96 */
+   double pixdim[8];     /*!< Grid spacings.        */ /* float pixdim[8];      */   /* 104 */
+   int64_t vox_offset;   /*!< Offset into .nii file */ /* float vox_offset;     */   /* 168 */
+   double scl_slope ;    /*!< Data scaling: slope.  */ /* float scl_slope;      */   /* 176 */
+   double scl_inter ;    /*!< Data scaling: offset. */ /* float scl_inter;      */   /* 184 */
+   double cal_max;       /*!< Max display intensity */ /* float cal_max;        */   /* 192 */
+   double cal_min;       /*!< Min display intensity */ /* float cal_min;        */   /* 200 */
+   double slice_duration;/*!< Time for 1 slice.     */ /* float slice_duration; */   /* 208 */
+   double toffset;       /*!< Time axis shift.      */ /* float toffset;        */   /* 216 */
+   int64_t slice_start;  /*!< First slice index.    */ /* short slice_start;    */   /* 224 */
+   int64_t slice_end;    /*!< Last slice index.     */ /* short slice_end;      */   /* 232 */
+   char  descrip[80];    /*!< any text you like.    */ /* char descrip[80];     */   /* 240 */
+   char  aux_file[24];   /*!< auxiliary filename.   */ /* char aux_file[24];    */   /* 320 */
+   int qform_code ;      /*!< NIFTI_XFORM_* code.   */ /* short qform_code;     */   /* 344 */
+   int sform_code ;      /*!< NIFTI_XFORM_* code.   */ /* short sform_code;     */   /* 348 */
+   double quatern_b ;    /*!< Quaternion b param.   */ /* float quatern_b;      */   /* 352 */
+   double quatern_c ;    /*!< Quaternion c param.   */ /* float quatern_c;      */   /* 360 */
+   double quatern_d ;    /*!< Quaternion d param.   */ /* float quatern_d;      */   /* 368 */
+   double qoffset_x ;    /*!< Quaternion x shift.   */ /* float qoffset_x;      */   /* 376 */
+   double qoffset_y ;    /*!< Quaternion y shift.   */ /* float qoffset_y;      */   /* 384 */
+   double qoffset_z ;    /*!< Quaternion z shift.   */ /* float qoffset_z;      */   /* 392 */
+   double srow_x[4] ;    /*!< 1st row affine transform. */  /* float srow_x[4]; */   /* 400 */
+   double srow_y[4] ;    /*!< 2nd row affine transform. */  /* float srow_y[4]; */   /* 432 */
+   double srow_z[4] ;    /*!< 3rd row affine transform. */  /* float srow_z[4]; */   /* 464 */
+   int slice_code ;      /*!< Slice timing order.   */ /* char slice_code;      */   /* 496 */
+   int xyzt_units ;      /*!< Units of pixdim[1..4] */ /* char xyzt_units;      */   /* 500 */
+   int intent_code ;     /*!< NIFTI_INTENT_* code.  */ /* short intent_code;    */   /* 504 */
+   char intent_name[16]; /*!< 'name' or meaning of data. */ /* char intent_name[16]; */  /* 508 */
+   char dim_info;        /*!< MRI slice ordering.   */      /* char dim_info;        */  /* 524 */
+   char unused_str[15];  /*!< unused, filled with \0 */                                  /* 525 */
+   } ;
+*)
+ImportNifTI2Header[stream_, opts___Rule] := Check[
+  SetStreamPosition[stream, 40];
+  Block[
+    {$ByteOrdering = If[0 < BinaryRead[stream, "Integer64"] < 8, 1, -1] * $ByteOrdering},
+    SetStreamPosition[stream, 0];
+    With[
+      {raw = ReadBinaryStructure[
+         stream,
+         {"HeaderSize" -> {"Integer32", 1, Function[
+            If[#[[1]] != $NifTI2HeaderSize, 
+              Message[ImportNifTI::badfmt, "Header does not start with 540"],
+              #[[1]]]]},
+          "MagicString" -> {"Character8", 4, Function[
+            With[
+              {str = BinaryStringFix[#[[1;;4]]]},
+              If[str == "ni2" || str == "n+2", 
+                str,
+                Message[ImportNifTI::badfmt, "header does not end in nil or n+1 string"]]]]},
+          "MagicSignature" -> {"Integer8", 4, Function[
+            If[# == {13, 10, 26, 10},
+              #,
+              Message[ImportNifTI::badfmt, "nifti2 magic signature invalid"]]]},
+          "Datatype" -> "Integer16",
+          "BitsPerVoxel" -> "Integer16",
+          "Dimensions" -> {"Integer64", 8, #[[2 ;; (#[[1]] + 1)]]&},
+          "IntentParameters" -> {"Real64", 3},
+          "GridSpacings" -> {"Real64", 8},
+          "VoxelOffset" -> {"Integer64", 1, Max[{#[[1]], $MinNifTI2Offset}]&},          
+          "ScaleSlope" -> "Real64",
+          "ScaleIntercept" -> "Real64",
+          "DisplayIntensityRange" -> {"Real64", 2, Reverse},
+          "SliceDuration" -> "Real64",
+          "TimeAxisShift" -> "Real64",
+          "SliceStart" -> "Integer64",
+          "SliceEnd" -> "Integer64",
+          "Description" -> {"Character8", 80, BinaryStringFix},
+          "AuxiliaryFilename" -> {"Character8", 24, BinaryStringFix},
+          "QFormCode" -> "Integer32",
+          "SFormCode" -> "Integer32",
+          "Quaternions" -> {"Real64", 6},
+          "AffineTransform" -> {"Real64", 12, Partition[#, 4]&},
+          "SliceCode" -> "Integer32",
+          "DimensionsUnits" -> {"Integer32", 1, NifTIUnitTranslate[#[[1]]]&},
+          "IntentCode" -> "Integer32",
+          "IntentName" -> {"Character8", 16, BinaryStringFix},
+          "DimensionsInformation" -> "Integer8",
+          "UnusedTerminus" -> {"Character8", 15}}]},
+      If[raw === $Failed,
+        raw,
+        "Header" -> Join[
+          With[
+            {dims = "Dimensions" /. raw},
+            Replace[
+              raw, 
+              ("GridSpacings" -> gs_) :> ("GridSpacings" -> gs[[2 ;; (Length[dims] + 1)]]),
+              {1}]],
+          {"ByteOrdering" -> $ByteOrdering,
+           "Extension" -> With[
+             {ext = NestWhileList[
+                Function @ With[
+                  {esize = BinaryRead[stream, "Integer32"],
+                   ecode = BinaryRead[stream, "Integer32"]},
+                  {Which[
+                    ecode == $NifTIExtensionCodeCifTI, ImportString[
+                      StringJoin @ BinaryReadList[stream, "Character8", esize - 12],
+                      "XML"],
+                    True, BinaryReadList[stream, "UnsignedInteger8", esize - 12]],
+                   BinaryReadList[stream, "Integer8", 4]}],
+                {{}, BinaryReadList[stream, "Integer8", 4]},
+                #[[2,1]] != 0 &]},
+             If[Length[ext] == 1, None, ext[[2;;All, 1]]]]}]]]],
+  $Failed];
+
+ImportNifTIHeader[stream_, opts___Rule] := Check[
+  SetStreamPosition[stream, 0];
+  With[
+    {sz = BinaryRead[stream, "Integer32"]},
+    Which[
+      sz == $NifTI1HeaderSize, ImportNifTI1Header[stream, opts],
+      sz == $NifTI2HeaderSize, ImportNifTI2Header[stream, opts],
+      True, Message[ImportNifTI::badfmt, "Header size is invalid!"]]],
   $Failed];
 
 ImportNifTIMetaInformation[stream_, opts___Rules] := Check[
@@ -373,11 +523,11 @@ ImportNifTIVoxels[stream_, opts___Rule] := Check[
        bitpix = "BitsPerVoxel" /. header,
        datatype = "Datatype" /. header,
        dims = "Dimensions" /. header},
-      SetStreamPosition[stream, offset];
+      SetStreamPosition[stream, Round[offset]];
       With[
         {raw = BinaryReadList[
            stream,
-           NifTIDataToBinaryType[datatype],
+           NifTIDatatypeTranslate[datatype],
            Times @@ dims]},
         "Voxels" -> NifTIColorTranslate[
           datatype,
@@ -417,7 +567,8 @@ ImportNifTI[stream_, opts___Rule] := Check[
     InterpretNifTI[data]],
   $Failed];
 
-Protect[ImportNifTIHeader, ImportNifTIVoxels, ImportNifTIMetaInformation, 
+Protect[ImportNifTIHeader, ImportNifTI1Header, ImportNifTI2Header,
+        ImportNifTIVoxels, ImportNifTIMetaInformation, 
         ImportNifTI, InterpretNifTI];
 
 ImportExport`RegisterImport[
