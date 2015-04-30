e3f772e3702a09b43b20d2140bb19a86accfcce6
Noah C. Benson <nben@nyu.edu>
2015-04-30 11:52:03 -0400

    Fixed a bug in the loading of NifTI voxels.

--- src/Neurotica/NifTI.m
+++ src/Neurotica/NifTI.m
@@ -539,8 +539,7 @@ ImportNifTIVoxels[stream_, opts___Rule] := Check[
             Fold[
               Partition,
               raw,
-              Most @ Reverse[
-                If[Length[dims] >= 4 && dims[[4]] == 1, Delete[dims, 4], dims]]]]]]]],
+              Most @ If[Length[dims] >= 4 && dims[[4]] == 1, Delete[dims, 4], dims]]]]]]],
   $Failed];
 
 ImportNifTIData[stream_, opts___Rule] := Check[
